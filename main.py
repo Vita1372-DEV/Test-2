318e4f138730e5cbb63330ea539d06886d0286d2
Alexander Leon Lopez <alexleonlopez2009@gmail.com>
2026-01-19 20:06:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hi :D")
+# EN PROCESO, AUN HAY ERRORES
+
+from datetime import date
+hoy = date.today()
+nombre = input("Ingrese su nombre: ")
+objetivo = input("¿En que actividad deseas avanzar?: ")
+while True:
+    try:
+        dias = int(input(f"Bien, {nombre} ¿cuál es tu objetivo semanal?: "))
+        break
+    except ValueError:
+        print("Por favor ingresa un número válido.")
+
+datos = {
+    "nombre_usuario": nombre,
+    "actividad": objetivo,
+    "objetivo_semanal": dias,
+    "hecho": 0,
+}
+print(f"Entonces deseas hacer {objetivo} {dias} veces a la semana.")
+while True:
+
+    respuesta = input(f"Hoy, {hoy} hiciste {objetivo}?\n SI/NO\n")
+    if respuesta.lower() == "si":
+        print(f"¡Excelente!")
+        datos["hecho"] += 1
+    elif respuesta.lower() == "no":
+        print("No te preocupes, mañana es un nuevo día para intentarlo.")
+    else:
+        print("Respuesta no válida. Por favor responde con SI o NO.")
