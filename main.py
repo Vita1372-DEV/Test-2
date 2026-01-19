e216d5be683e8e3530f310408ccf53a5bd6d4954
Alexander Leon Lopez <alexleonlopez2009@gmail.com>
2026-01-19 20:07:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # EN PROCESO, AUN HAY ERRORES
-
 from datetime import date
+
 hoy = date.today()
+
 nombre = input("Ingrese su nombre: ")
 objetivo = input("¿En que actividad deseas avanzar?: ")
+
 while True:
     try:
         dias = int(input(f"Bien, {nombre} ¿cuál es tu objetivo semanal?: "))
@@ -17,14 +19,23 @@ datos = {
     "objetivo_semanal": dias,
     "hecho": 0,
 }
-print(f"Entonces deseas hacer {objetivo} {dias} veces a la semana.")
-while True:
 
-    respuesta = input(f"Hoy, {hoy} hiciste {objetivo}?\n SI/NO\n")
+print(f"Entonces deseas hacer {objetivo} {dias} veces a la semana.")
+
+while True:
+    respuesta = input(f"Hoy ({hoy}) hiciste {objetivo}? (si/no): ")
+
     if respuesta.lower() == "si":
-        print(f"¡Excelente!")
+        print("¡Excelente!")
         datos["hecho"] += 1
+        break
     elif respuesta.lower() == "no":
         print("No te preocupes, mañana es un nuevo día para intentarlo.")
+        break
     else:
-        print("Respuesta no válida. Por favor responde con SI o NO.")
+        print("Respuesta no válida. Por favor responde con si o no.")
+
+print(
+    f"Llevas {datos['hecho']} de {datos['objetivo_semanal']} "
+    f"veces esta semana."
+)
